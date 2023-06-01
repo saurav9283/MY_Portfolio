f6a53d9c346f2b6a9aebd1a0bff9db8f297e32b6
Saurav <sauravkumar00108@gmail.com>
2023-06-02 02:50:15 +0530

    first commit

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -5,7 +5,7 @@ export const ProjectCard = ({ title, description, imgUrl, link }) => {
     <Col size={12} sm={6} md={4}>
       <a href={link} target="_blank">
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt="err"/>
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
