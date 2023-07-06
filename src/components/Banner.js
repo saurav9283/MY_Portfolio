ddf7df6918375d08dec79fe797d55de9568f4f55
Saurav <sauravkumar00108@gmail.com>
2023-07-06 17:30:08 +0530

    first commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
   const [activeLink, setActiveLink] = useState("contact");
-  const toRotate = ["Software Developer", "Web Developer", "Data Analytics"];
+  // const toRotate = ["Software Developer"];
   const [showpopup, setshowpopup] = useState(false);
   const period = 2000;
 
@@ -31,30 +31,30 @@ export const Banner = () => {
   }, [text]);
 
   const tick = () => {
-    let i = loopNum % toRotate.length;
-    let fullText = toRotate[i];
-    let updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
+    // let i = loopNum % toRotate.length;
+    // let fullText = toRotate[i];
+    // let updatedText = isDeleting
+      // ? fullText.substring(0, text.length - 1)
+      // : fullText.substring(0, text.length + 1);
 
-    setText(updatedText);
+    // setText(updatedText);
 
     if (isDeleting === true) {
       setDelta((prevDelta) => prevDelta / 2);
     }
 
-    if (isDeleting === false && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex((prevIndex) => prevIndex - 1);
-      setDelta(period);
-    } else if (isDeleting && updatedText === "") {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
-    } else {
-      setIndex((prevIndex) => prevIndex + 1);
-    }
+    // if (isDeleting === false && updatedText === fullText) {
+    //   setIsDeleting(true);
+    //   setIndex((prevIndex) => prevIndex - 1);
+    //   setDelta(period);
+    // } else if (isDeleting && updatedText === "") {
+    //   setIsDeleting(false);
+    //   setLoopNum(loopNum + 1);
+    //   setIndex(1);
+    //   setDelta(500);
+    // } else {
+    //   setIndex((prevIndex) => prevIndex + 1);
+    // }
   };
 
   return (
@@ -65,19 +65,21 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) => (
                 <div
-                  className={
-                    isVisible ? "animate__animated animate__fadeIn" : ""
-                  }
+                  // className={
+                  //   isVisible ? "animate__animated animate__fadeIn" : ""
+                  // }
                 >
                   <h1>
-                    {`Hi! I'm Saurav,`}{" "}
-                    <span
+                    {/* {`Hi! I'm Saurav,`}{""} */}
+                    {`Hi! I'm Saurav,`}<br/>
+                    Software Developer
+                    {/* <span
                       className="txt-rotate"
                       dataperiod="800"
-                      data-rotate='[ "Software Developer", "Web Developer", "Data Analytics" ]'
+                      data-rotate='[ "Software Developer"]'
                     >
                       <span className="wrap">{text}</span>
-                    </span>
+                    </span> */}
                   </h1>
                   <button onClick={handelPopup}>
                     More About Me <ArrowRightCircle size={20} />
